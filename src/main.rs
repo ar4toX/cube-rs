757252349afa735d274fb792a701910fcdb45b23
aratof <aratof@framework.localdomain>
2024-07-30 00:36:31 +0000

    Began raylib doing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init()
+        .size(700, 500)
+        .title("wine to rev your motor functions")
+        .build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::BLACK);
+        d.draw_text("Test 1: Successful", 12, 12, 20, Color::WHITE);
+    }
 }
